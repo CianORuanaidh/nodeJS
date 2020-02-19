bffca30b3411555faffe5e59e962676c97e707fc
Cian O'Ruanaidh <cianor.social@gmail.com>
2020-02-18 20:55:40 -0500

    updates

--- express-first-app-handlebas/app.js
+++ express-first-app-handlebas/app.js
@@ -3,7 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var hbs = require('express-handlebars')
+var hbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var expressSession = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,8 +20,11 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+// app.use(expressValidator());
+app.use(expressValidator())
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressSession({ secret: 'max', saveUninitialized: false, resave: false }))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- express-first-app-handlebas/routes/index.js
+++ express-first-app-handlebas/routes/index.js
@@ -2,17 +2,39 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', condition: false, anyArray: [1,2,3] });
+  res.render('index', { title: 'Form Validation', success: req.session.success, errors: req.session.errors });
+  req.session.errors = null;
 });
 
-router.get('/test/:id', function( req, res, next){
-  res.render('test', { output: req.params.id })
+router.post('/submit', function(req, res, next){
+  // Check Validity
+  req.check('email', 'Invalid email address').isEmail();
+  req.check('password', 'Password is Invalid').isLength({min:4}).equals(req.body.confirmPassword)
+
+  var errors = req.validationErrors();
+  if (errors) {
+    req.session.errors = errors;
+    req.session.success = false;
+  } else {
+    req.session.success = true;
+  }
+  res.redirect('/');
+
 });
 
-router.post('/test/submit', function( req, res, next){
-  var id = req.body.id;
-  res.redirect('/test/' + id)
-})
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express', condition: false, anyArray: [1,2,3] });
+// });
+
+// router.get('/test/:id', function( req, res, next){
+//   res.render('test', { output: req.params.id })
+// });
+
+// router.post('/test/submit', function( req, res, next){
+//   var id = req.body.id;
+//   res.redirect('/test/' + id)
+// })
 
 module.exports = router;
